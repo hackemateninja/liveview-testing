b500510c6aa07a0c3877c03585221c2f586ea211
Herman <nitrowitty@gmail.com>
2024-12-05 03:04:39 -0600

    testing form changes

--- test/ranger_web/live/newsletter_live_test.exs
+++ test/ranger_web/live/newsletter_live_test.exs
@@ -1,5 +1,30 @@
 defmodule RangerWeb.NewsletterLiveTest do
-  use RangerWeb.ConnCase
+  use RangerWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
+
+  test "Wanrs user of invalid email as user input", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/newsletter")
+
+    invalid_email = "something"
+
+    html =
+      view
+      |> form("#subscribe", %{subscription: %{email: invalid_email}})
+      |> render_change()
+
+    assert html =~ "has invalid format"
+  end
+
+  test "Warns user of invalid email as user types (directly event)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/newsletter")
+
+    invalid_email = "something"
+
+    html =
+      view
+      |> render_change("validate", %{subscription: %{email: invalid_email}})
+
+    assert html =~ "has invalid format"
+  end
 end
