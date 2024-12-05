870d1e2b51e4fa533d7e860b89f46326462afb59
Herman <nitrowitty@gmail.com>
2024-12-04 22:21:05 -0600

    testing interaction and open browser

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -1,5 +1,77 @@
 defmodule RangerWeb.CounterLiveTest do
-  use RangerWeb.ConnCase
+  use RangerWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
+
+  test "user can increase counter", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  test "user can increase counter (uses HTML)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    html =
+      view
+      |> element("#increment")
+      |> render_click()
+
+    assert html =~ "1"
+  end
+
+  test "user can increase counter (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view |> render_click("increase")
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  test "debuggins liveview tests", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view |> open_browser()
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    view |> open_browser()
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  test "user can decrease counter", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#decrement")
+    |> render_click()
+
+    assert has_element?(view, "#count", "-1")
+  end
+
+  test "user can decrease counter (uses HTML)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    html =
+      view
+      |> element("#increment")
+      |> render_click()
+
+    assert html =~ "-1"
+  end
+
+  test "user can decrease counter (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view |> render_click("decrease")
+
+    assert has_element?(view, "#count", "-1")
+  end
 end
