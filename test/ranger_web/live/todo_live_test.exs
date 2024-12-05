1a6408cd2c50e12fb7d22c08967fb5c30e139afc
Herman <nitrowitty@gmail.com>
2024-12-05 02:45:15 -0600

    test form

--- test/ranger_web/live/todo_live_test.exs
+++ test/ranger_web/live/todo_live_test.exs
@@ -1,5 +1,34 @@
 defmodule RangerWeb.TodoLiveTest do
-  use RangerWeb.ConnCase
+  use RangerWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
+
+  test "user can creeate a new todo", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/todo")
+
+    view
+    |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+    |> render_submit()
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+  end
+
+  test "user can create a new todo (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/todo")
+
+    view |> render_submit("create", %{todo: %{body: "Form fellowship"}})
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+  end
+
+  test "user can create a new todo (width HTML)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/todo")
+
+    html =
+      view
+      |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+      |> render_submit()
+
+    assert html =~ "Form fellowship"
+  end
 end
